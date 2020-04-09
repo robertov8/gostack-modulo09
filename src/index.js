6f780aee69ca079bad90ff2f60f2bc87d29e28fe
Roberto Ribeiro <robertov82008@gmail.com>
2020-04-08 23:06:12 -0300

    03 - Rotas de autenticação

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Routes from './routes';
+
 // import { Container } from './styles';
 
 export default function App() {
-  return <View />;
+  return <Routes />;
 }
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
