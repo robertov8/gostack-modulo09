1f4be9d6643ecc973d69ad952b3c4872b368c8ea
Roberto Ribeiro <robertov82008@gmail.com>
2020-04-18 18:51:28 -0300

    14 - Estilizações das rotas

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,50 @@ import PropTypes from 'prop-types';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function App() {
   return (
-    <Tab.Navigator tabBarOptions={{ tabStyle: { justifyContent: 'center' } }}>
-      <Tab.Screen name="App" component={Dashboard} />
+    <Tab.Navigator
+      tabBarOptions={{
+        keyboardHidesTabBar: true,
+        activeTintColor: '#fff',
+        inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+        style: {
+          borderTopColor: '#8d41a8',
+          backgroundColor: '#8d41a8',
+        },
+      }}>
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarLabel: 'Agendamentos',
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="event" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Meu perfil',
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="person" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
