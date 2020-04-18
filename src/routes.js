254cb52172972f8a8a76129f4ad206608f14fcfd
Roberto Ribeiro <robertov82008@gmail.com>
2020-04-18 18:24:23 -0300

    13 - Rota inicial

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
-const disableHeader = {};
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function Routes() {
+function App() {
+  return (
+    <Tab.Navigator tabBarOptions={{ tabStyle: { justifyContent: 'center' } }}>
+      <Tab.Screen name="App" component={Dashboard} />
+    </Tab.Navigator>
+  );
+}
+
+export default function Routes({ signedIn }) {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SignIn"
-          component={SignIn}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{ headerShown: false }}
-        />
+      <Stack.Navigator initialRouteName={signedIn ? 'App' : 'SignIn'}>
+        {signedIn ? (
+          <Stack.Screen
+            name="App"
+            component={App}
+            options={{ headerShown: false }}
+          />
+        ) : (
+          <>
+            <Stack.Screen
+              name="SignIn"
+              component={SignIn}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="SignUp"
+              component={SignUp}
+              options={{ headerShown: false }}
+            />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+Routes.propTypes = {
+  signedIn: PropTypes.bool,
+};
+
+Routes.defaultProps = {
+  signedIn: false,
+};
